275e6895ac90b26c5f03510ca6192849b749825f
Yuvraj Sheoran <98250164+yuvraj-sheoran@users.noreply.github.com>
2022-12-06 22:15:12 +0530

    Shell Script Program - 3

---
+++ Check whether given number is prime or not.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+num="enter number"
+read num
+function prime
+{
+for((i=2; i<=num/2; i++))
+do
+  if [ $((num%i)) -eq 0 ]
+  then
+    echo "$num is not a prime number."
+    exit
+  fi
+done
+echo "$num is a prime number."
+}
+r=`prime $number`
+echo "$r"
