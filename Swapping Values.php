4b7ae6fa0380b12a62ecd8bca7fb170c5459a126
Yuvraj Sheoran <98250164+yuvraj-sheoran@users.noreply.github.com>
2022-12-06 21:51:19 +0530

    PHP Program - 10

---
+++ Swapping Values.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+ 
+echo "Before Swapping:<br>";
+$a = 1;  
+$b = 2;
+echo "a = $a<br>";
+echo "b = $b<br><br>";
+ 
+echo "After swapping:<br>";  
+$temp = $a;  
+$a = $b;  
+$b = $temp;  
+ 
+echo "a = $a<br>";
+echo "b = $b<br>";
+?> 
+ 
+</body>
+</html>
